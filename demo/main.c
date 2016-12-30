839543e2ca0c872e55bd02a9ceabb3705281e11c
Ivan Nikolaev <voidexp@gmail.com>
2016-12-31 00:32:44 +0100

    Show FPS and render time stats

--- demo/main.c
+++ demo/main.c
@@ -3,6 +3,7 @@
 #include <matlib.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 // renderlib headers
 #include "renderer.h"
@@ -11,6 +12,7 @@
 
 #define WIDTH 800
 #define HEIGHT 600
+#define UPDATE_INTERVAL 2.0f
 
 static SDL_Window *window = NULL;
 static SDL_GLContext *context = NULL;
@@ -51,11 +53,12 @@ init(unsigned width, unsigned height)
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
 	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
-
 	context = SDL_GL_CreateContext(window);
 	if (!context) {
 		return 0;
 	}
+
+	SDL_GL_SetSwapInterval(0);
 
 	// one-off OpenGL initializations
 	glClearColor(0.3, 0.3, 0.3, 1.0);
@@ -97,7 +100,7 @@ shutdown(void)
 }
 
 static int
-update(void)
+update(float dt)
 {
 	SDL_Event evt;
 	while (SDL_PollEvent(&evt)) {
@@ -146,6 +149,36 @@ cleanup_resources(void)
 	mesh_free(mesh);
 }
 
+static void
+update_stats(int fps, float render_time)
+{
+	render_time *= 1000.f; // in milliseconds
+	const char *fmt = "FPS: %d, render time: %.2fms";
+	int len = snprintf(NULL, 0, fmt, fps, render_time) + 1;
+	char str[len + 1];
+	str[len] = '\0'; // NUL-terminator
+	snprintf(str, len, fmt, fps, render_time);
+	SDL_SetWindowTitle(window, str);
+}
+
+static struct timespec
+tstamp(void)
+{
+	struct timespec t;
+	clock_gettime(CLOCK_MONOTONIC_RAW, &t);
+	return t;
+}
+
+static float
+timedelta(struct timespec a, struct timespec b)
+{
+	unsigned t1_secs = a.tv_sec & 0x1ffff;
+	unsigned t2_secs = b.tv_sec & 0x1ffff;
+	unsigned long long t1 = t1_secs * 1e9 + a.tv_nsec;
+	unsigned long long t2 = t2_secs * 1e9 + b.tv_nsec;
+	return (t1 - t2) / 1e9f;
+}
+
 int
 main(int argc, char *argv[])
 {
@@ -155,9 +188,33 @@ main(int argc, char *argv[])
 	);
 
 	int run = 1;
+
+	struct timespec last_update = tstamp();
+	float time_acc = 0, render_time = 0;
+	int fps = 0;
 	while (ok && run) {
-		run &= update();
+		// compute time delta
+		struct timespec now = tstamp();
+		float dt = timedelta(now, last_update);
+		last_update = now;
+
+		// update stats
+		time_acc += dt;
+		if (time_acc >= UPDATE_INTERVAL) {
+			time_acc -= UPDATE_INTERVAL;
+			update_stats(fps / UPDATE_INTERVAL, render_time);
+			fps = 0;
+		} else {
+			fps++;
+		}
+
+		// update the scene
+		run &= update(dt);
+
+		// render and measure rendering time
+		now = tstamp();
 		run &= render();
+		render_time = timedelta(tstamp(), now);
 	}
 
 	cleanup_resources();
