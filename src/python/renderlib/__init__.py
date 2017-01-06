3ebb3f6b349443bf4eb106addab1683885fbd161
Ivan Nikolaev <voidexp@gmail.com>
2017-01-06 13:56:48 +0100

    Python wrapper package imports

--- src/python/renderlib/__init__.py
+++ src/python/renderlib/__init__.py
@@ -1,4 +1,6 @@
 from renderlib.core import renderer_init  # noqa
 from renderlib.core import renderer_present  # noqa
 from renderlib.core import renderer_shutdown  # noqa
+from renderlib.font import Font  # noqa
+from renderlib.image import Image  # noqa
 from renderlib.mesh import Mesh  # noqa
