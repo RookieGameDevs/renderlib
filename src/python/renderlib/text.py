e73ea80f1682f4fe8f3e3daf614722d83cf22cc6
Ivan Nikolaev <voidexp@gmail.com>
2017-01-10 21:47:08 +0100

    Avoid unnecessary string setting in Text

--- src/python/renderlib/text.py
+++ src/python/renderlib/text.py
@@ -5,6 +5,7 @@ class Text:
         self._ptr = lib.text_new(font._ptr)
         if not self._ptr:
             raise RuntimeError('failed to create text')
+        self._string = None
         self.string = string
 
     def __del__(self):
@@ -16,9 +17,10 @@ class Text:
 
     @string.setter
     def string(self, s):
-        self._string = s
-        if not lib.text_set_string(self._ptr, s.encode('utf8')):
-            raise RuntimeError('failed to set text string')
+        if self._string != s:
+            self._string = s
+            if not lib.text_set_string(self._ptr, s.encode('utf8')):
+                raise RuntimeError('failed to set text string')
 
     @property
     def width(self):
